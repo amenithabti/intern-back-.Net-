05b610ba853760cf1f2c183046d36e43bc5112e0
Ameni Thabti <amenithabti@yahoo.fr>
2021-08-26 22:50:24 +0200

    backend modif

---
+++ Backend-App/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_App.Models
+{
+    public class User
+    {
+        [Key]
+
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(30)")]
+
+        public string username { get; set; }
+
+        [Column(TypeName = "nvarchar(30)")]
+
+        public string password { get; set; }
+    }
+}
